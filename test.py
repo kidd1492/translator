b8582a24b3725b63072d28407d1d36d96b841b21
kidd1492 <chris.wr1492@gmail.com>
2025-03-10 13:31:23 -0400

    testing source control

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 import pyttsx3
 
-"""
 voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
 engine = pyttsx3.init()
 engine.setProperty('voice', voice_id)
 engine.say("Hello, I can speak offline!")
 engine.runAndWait()
-"""
+
 
 help(pyttsx3.engine)
